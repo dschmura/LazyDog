b727ad1def374a2c2da1956b6bb954b0ec0bf3e1
David Chmura <dschmura@humbledaisy.com>
2018-04-05 15:06:47 -0400

    UPDATE: link the nginx file.

--- baseapp/config/deploy.rb
+++ baseapp/config/deploy.rb
@@ -108,6 +108,6 @@
   # kill -s SIGTERM pid   # Stop puma
   ## Linked Files & Directories (Default None):
 
-  set :linked_files, %w{config/puma.rb config/master.key}
+  set :linked_files, %w{config/puma.rb config/nginx.conf config/master.key}
   set :linked_dirs,  %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
   set :linked_dirs, fetch(:linked_dirs, []).push('public/packs', 'node_modules')
