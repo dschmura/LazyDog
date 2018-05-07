1a881399715795f1de570998a981ff25241c6663
David Chmura <dschmura@humbledaisy.com>
2018-05-07 09:26:22 -0400

    FIX: Deprecation warnings on pages_controller_spec
    DEPRECATION WARNING: The success? predicate is deprecated and will be removed in Rails 6.0. Please use successful? as provided by Rack::Response::Helpers

--- baseapp/spec/controllers/pages_controller_spec.rb
+++ baseapp/spec/controllers/pages_controller_spec.rb
@@ -5,28 +5,35 @@ RSpec.describe PagesController, type: :controller do
   describe "GET #index" do
     it "returns http success" do
       get :index
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:successful?)
     end
   end
 
   describe "GET #about" do
     it "returns http success" do
       get :about
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:successful?)
     end
   end
 
   describe "GET #contact" do
     it "returns http success" do
       get :contact
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:successful?)
     end
   end
 
   describe "GET #privacy" do
     it "returns http success" do
       get :privacy
-      expect(response).to have_http_status(:success)
+      expect(response).to have_http_status(:successful?)
+    end
+  end
+
+  describe "GET #project_status" do
+    it "returns http success" do
+      get :project_status
+      expect(response).to have_http_status(:successful?)
     end
   end
 
