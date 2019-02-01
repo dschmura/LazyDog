cf33a3126ba6efe4224f1ba60ed0541ce49889c4
David Chmura <dschmura@humbledaisy.com>
2019-02-01 17:38:09 -0500

    ADD: flash and errors styling using tailwindcss

---
+++ baseapp/app/helpers/flash_errors_helper.rb
@@ -0,0 +1,17 @@
+module FlashErrorsHelper
+  def flash_class(level)
+    case level
+    when :danger   then 'alert alert-danger'
+    when :error   then 'alert alert-error'
+    when :notice  then 'alert alert-notice'
+    when :success then 'alert alert-success'
+    when :warning   then 'alert alert-warning'
+    end
+  end
+
+  SEVERITY_ICONS = {notice: 'info-circle', danger: 'exclamation-triangle',  error: 'exclamation', success: 'check-circle', warning: 'bullhorn' }
+
+  def severity_icon(severity)
+    SEVERITY_ICONS[severity]
+  end
+end
