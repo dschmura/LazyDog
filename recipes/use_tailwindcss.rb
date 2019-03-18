91321c7fa558ddfafd24e193cb8fbbf0b18e76d1
David Chmura <dschmura@humbledaisy.com>
2019-03-18 16:21:52 -0400

    FIX: add_tailwinds recipe

--- recipes/use_tailwindcss.rb
+++ recipes/use_tailwindcss.rb
@@ -1,12 +1,14 @@
 #  https://www.artmann.co/articles/adding-tailwind-css-to-your-rails-app
 run 'yarn add --dev autoprefixer tailwindcss'
-run './node_modules/.bin/tailwind init app/javascript/css'
+run './node_modules/.bin/tailwind init app/javascript/css/tailwind.js'
 
-insert_into_file 'postcss.config.js', after: "module.exports = {\n" do
-  var tailwindcss = require('tailwindcss');
+insert_into_file 'postcss.config.js', before: "module.exports = {\n" do
+  <<-POSTCSS_config
+var tailwindcss = require('tailwindcss');
+  POSTCSS_config
 end
 
-insert_into_file 'postcss.config.js', before: "plugins: [\n" do
+insert_into_file 'postcss.config.js', after: "plugins: [\n" do
   <<-POSTCSS_config
     tailwindcss('./app/javascript/css/tailwind.js'),
     require('autoprefixer'),
