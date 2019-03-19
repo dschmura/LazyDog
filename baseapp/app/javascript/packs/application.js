c27ab30f79088588ab92d8862dbee2100f0257c9
David Chmura <dschmura@humbledaisy.com>
2019-03-19 09:31:24 -0400

    UPDATE: Link sass files

--- baseapp/app/javascript/packs/application.js
+++ baseapp/app/javascript/packs/application.js
@@ -1,25 +1,28 @@
-/* eslint no-console:0 */
 // This file is automatically compiled by Webpack, along with any other files
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
 
-  // import $ from 'jquery'
-  // global.$ = $
-  // global.jQuery = $
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
 
-  import Rails from 'rails-ujs'
-  Rails.start()
+var Turbolinks = require("turbolinks")
+Turbolinks.start()
 
-  import Turbolinks from 'turbolinks'
-  Turbolinks.start()
+// Specific frontend applications
+import 'app_name'
 
-  // Specific frontend applications
-  import 'app_name'
+import 'app_name/stylesheets/_variables.sass'
+import 'app_name/stylesheets/application.sass'
+import 'app_name/stylesheets/_header.sass'
+import 'app_name/stylesheets/_flash_errors.sass'
+import 'app_name/stylesheets/_footer.sass'
+import 'app_name/stylesheets/ribbons.sass'
 
-  document.addEventListener('turbolinks:load', () => {
-    // FontAwesome.dom.i2svg();
-  })
+import '@fortawesome/fontawesome-free/js/all';
+require.context('../app_name/images/', true, /.(gif|jpg|jpeg|png|svg)$/)
+
+document.addEventListener('turbolinks:load', () => {
+  // FontAwesome.dom.i2svg();
+})
