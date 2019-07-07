5dc552949580ef1a87b22d37dc2aaa4664a8c995
David Chmura <dschmura@humbledaisy.com>
2019-07-07 09:59:03 -0400

    UPDATE: Lots of style things

--- recipes/use_tailwindcss.rb
+++ recipes/use_tailwindcss.rb
@@ -2,6 +2,8 @@
 # run 'yarn add --dev autoprefixer tailwindcss'
 run 'yarn add -D tailwindcss@next'
 
+run 'yarn add @tailwindcss/custom-forms'
+
 run './node_modules/.bin/tailwind init app/javascript/css/tailwind.config.js'
 
 insert_into_file 'postcss.config.js', before: "module.exports = {\n" do
@@ -16,3 +18,9 @@ insert_into_file 'postcss.config.js', after: "plugins: [\n" do
     require('autoprefixer'),
   POSTCSS_config
 end
+
+insert_into_file 'app/javascript/css/tailwind.config.js', after: "plugins: [\n" do
+  <<-TAILWINDCSS_CUSTOM_FORMS
+    require('@tailwindcss/custom-forms')
+  TAILWINDCSS_CUSTOM_FORMS
+  end
