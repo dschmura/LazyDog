d4569f4ff18a0df9f8218674abd0860df2bce7c7
David Chmura <dschmura@humbledaisy.com>
2019-11-06 09:53:31 -0500

    REFACTOR: use rails_env to determine which config files to upload to the server.
    This addresses (abeit crudely) the issue where the puma config for production was getting loaded onto staging

--- baseapp/config/deploy.rb
+++ baseapp/config/deploy.rb
@@ -78,8 +78,8 @@ namespace :deploy do
   task :upload do
     on roles :app do
       upload! "config/master.key",  "#{shared_path}/config/master.key"
-      upload! "config/puma.sample.rb", "#{shared_path}/config/puma.rb"
-      upload! "config/nginx.sample.conf", "#{shared_path}/config/nginx.conf"
+      upload! "config/puma.#{:rails_env}.rb", "#{shared_path}/config/puma.rb"
+      upload! "config/nginx.#{:rails_env}.conf", "#{shared_path}/config/nginx.conf"
     end
   end
 
