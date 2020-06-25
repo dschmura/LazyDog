b502b05867a96559eea4bb4caec7a2ef3e3bad05
David Chmura <dschmura@humbledaisy.com>
2020-06-25 12:05:43 -0400

    REFACTOR: updating how rspec gets added

---
+++ recipes/use_rspec.rb
@@ -0,0 +1,40 @@
+ # Bundle and set up RSpec
+ gem_group :development, :test do
+  gem "rspec-rails"
+end
+
+ run 'bundle exec rails generate rspec:install'
+ run 'guard init'
+
+ insert_into_file 'spec/rails_helper.rb', after: 'RSpec.configure do |config|\n' do
+  <<~EOF
+  config.include Warden::Test::Helpers
+
+  EOF
+
+ end
+ if
+
+insert_into_file 'config/application.rb', after: 'config.generators.system_tests = nil\n' do
+  <<-CONFIG_RSPEC
+
+  config.generators do |g|
+    g.javascript_engine :js
+    g.helper false
+
+    g.test_framework :rspec,
+      fixtures: false,
+      view_specs: false,
+      helper_specs: false,
+      routing_specs: false,
+      request_specs: false,
+      controller_specs: false
+  end
+
+  CONFIG_RSPEC
+end
+
+
+file .rspec unless File.exist?("./.rspec")
+
+append_to_file .rspec { "--require spec_helper" }
